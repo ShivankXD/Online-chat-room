67c53f8cd6914d2726b062fbfa56d0194c297379
ShivankXD <shivankprabhudessai@gmail.com>
2024-11-07 20:27:28 +0530

    Created Database connection using php

---
+++ db_connect.php
@@ -0,0 +1,22 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$database="chatroom";
+
+
+//database connection 
+$conn =mysqli_connect($servername,$username,$password,$database);
+
+
+//checking connection
+if(!$conn)
+{
+    die("Failed to connect". mysqli_connect_error());
+
+}
+
+
+?>
+
