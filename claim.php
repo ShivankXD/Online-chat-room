3f0a4405e1ecaba713a27bfc578c709cb81ad952
ShivankXD <shivankprabhudessai@gmail.com>
2024-11-09 22:05:03 +0530

    Created a database for storing room name and room timestamp

--- claim.php
+++ claim.php
@@ -26,7 +26,40 @@
     include 'db_connect.php'; 
   }
 
-  //for verifiying if connection is done properly
-  echo "Let's chat now!";
+  // //for verifiying if connection is done properly
+  // echo "Let's chat now!";
 
+  //check if room aldready exists 
+  $sql = "SELECT * FROM 'rooms' WHERE roomname ='$room'";
+  $result=mysqli_query($conn,$sql);
+  if($result)
+  {
+     if(mysqli_num_rows($result)>0)
+     {
+      $message= "Please Choose a different room name . This room is aldready claimed ";
+      echo '<script language="javascript">';
+      echo 'alert(" '.$message.'");';
+      echo 'window.location="http://localhost/ONLINECHATROOM";';
+      echo '</script>';
+     }
+     
+
+     //if room is not claimed 
+     else{
+        $sql="INSERT INTO `rooms` (`sno`, `roomname`, `stime`) VALUES ('1', '$room', '2024-11-09 16:40:02.000000');";
+        if(mysqli_query($conn,$sql))
+        {
+          $message= "Your room is ready and you can chat now!";
+          echo '<script language="javascript">';
+          echo 'alert(" '.$message.'");';
+          echo 'window.location="http://localhost/ONLINECHATROOM/rooms.php?roomname= ' .$room. '";';
+          echo '</script>';
+        }
+
+     }
+  }
+  else {
+    echo "Error:". mysqli_error($conn);
+
+  }
   ?>
